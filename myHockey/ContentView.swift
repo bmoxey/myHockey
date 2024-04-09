d547ab7a5152aa8d31f8c9014ec5a3950f55bdfa
bmoxey <bmoxey@gmail.com>
2024-04-09 16:10:03 +1000

    Initial Commit

---
+++ myHockey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myHockey
+//
+//  Created by Brett Moxey on 9/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
